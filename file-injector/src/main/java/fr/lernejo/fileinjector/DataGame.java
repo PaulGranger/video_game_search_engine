d90ef02058d69b7c5467a53367fd8a649ea42657
PaulGranger <paul.granger@epita.fr>
2022-02-06 11:23:32 +0100

    Partie 3 : modification laucher fileinjector + creation class dataGame

---
+++ file-injector/src/main/java/fr/lernejo/fileinjector/DataGame.java
@@ -0,0 +1,14 @@
+package fr.lernejo.fileinjector;
+
+public class DataGame {
+    public Integer id;
+    public String title;
+    public String thumbnail;
+    public String short_description;
+    public String genre;
+    public String platform;
+    public String publisher;
+    public String developer;
+    public String release_date;
+
+}
--- file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
+++ file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
@@ -1,16 +1,31 @@
 package fr.lernejo.fileinjector;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
 @SpringBootApplication
 public class Launcher {
-
-    public static void main(String[] args) {
-        try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
-
-            System.out.println("Hello after starting Spring");
+    public static void main(String[] args) throws IOException {
+        if (args.length > 0) {
+            try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
+                ObjectMapper objectMapper = new ObjectMapper();
+                DataGame[] dataGames = objectMapper.readValue(Paths.get(args[0]).toFile(), DataGame[].class);
+                RabbitTemplate rabbitTemplate = springContext.getBean(RabbitTemplate.class);
+                rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+                for (DataGame dataGame : dataGames) {
+                    rabbitTemplate.convertAndSend("", "game_info", dataGame, message -> {
+                        message.getMessageProperties().getHeaders().put("game_id", dataGame.id);
+                        return message;
+                    });
+                }
+            }
         }
     }
 }
